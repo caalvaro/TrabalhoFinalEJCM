0e0c24e36665592d97c24dc6cc2666a83e2a9a24
Alvaro <alvarocarvalho@live.com>
2019-08-13 22:41:29 -0300

    + tratamento de erro ao logar

--- ionic-blog/src/app/pages/login/login.page.ts
+++ ionic-blog/src/app/pages/login/login.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { ToastController } from '@ionic/angular';
+
 import { AuthService } from '../../service/auth.service';
 
 import { Router } from '@angular/router';
@@ -15,7 +17,8 @@ export class LoginPage implements OnInit {
   
   loginForm: FormGroup;
 
-  constructor( public formbuilder: FormBuilder, public authService: AuthService,  public router: Router ) { 
+  constructor( public formbuilder: FormBuilder, public authService: AuthService,  public router: Router,
+    public toastController: ToastController ) { 
     this.loginForm = this.formbuilder.group({
   		email: ['', [Validators.required, Validators.email]],
   		password: ['', [Validators.required, Validators.minLength(6)]],
@@ -25,13 +28,24 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
-  logarUsuario( loginForm: FormGroup) {
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Email ou senha não correspondem.',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  logarUsuario( loginForm: FormGroup ) {
     if ( loginForm.status == "VALID" ) {
       this.authService.logarUsuario(loginForm.value).subscribe(
-        (res) => {
+        ( res ) => {
           //console.log(res);
           localStorage.setItem('userToken', res.success.token);
           this.router.navigate(['tabs/home']);
+        },
+        ( err ) => {
+          this.presentToast();
         }
       );
     }
